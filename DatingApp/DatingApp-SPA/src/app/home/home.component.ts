6999aac34cb179e90ccae8ed382cb57b6920730f
Jayce77 <xjaycex@hotmail.com>
2018-11-27 22:10:25 -0500

    Added register method to service

--- DatingApp/DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp/DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,23 +8,13 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   registerMode = false;
   values: any;
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5000/api/values')
-      .subscribe(response => {
-        this.values = response;
-      }, error => {
-        console.log(error);
-      });
   }
 
   cancelRegisterMode(registerMode: boolean) {
