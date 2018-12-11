b1c84f0b89dc613cfd98d5d2b9b2167b44a70d5e
Jayce77 <xjaycex@hotmail.com>
2018-12-10 22:17:02 -0500

    Added resolvers for memberList and memberDetail components

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
 
 export const tokenGetter = () => localStorage.getItem('token');
 
@@ -58,7 +60,9 @@ export const tokenGetter = () => localStorage.getItem('token');
       ErrorInterceptorProvider,
       AlertifyService,
       AuthGuard,
-      UserService
+      UserService,
+      MemberDetailResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
