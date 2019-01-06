63f03150341b4bcd47003592620b92b33abccfad
Jayce77 <xjaycex@hotmail.com>
2019-01-06 11:26:32 -0500

    finished AddPhotoForUser route added GetPhoto route, model and mapping

--- DatingApp/DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp/DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -25,6 +25,8 @@ namespace DatingApp.API.Helpers
                 });
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
