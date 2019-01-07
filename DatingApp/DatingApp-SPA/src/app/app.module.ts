94beb12c37bfd68c742732b22f5c607d198e5504
Jayce77 <xjaycex@hotmail.com>
2019-01-06 22:13:58 -0500

    added PhotoEditor component

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 export const tokenGetter = () => localStorage.getItem('token');
 
@@ -42,7 +43,8 @@ export const tokenGetter = () => localStorage.getItem('token');
       MessagesComponent,
       MemberCardComponent,
       MemberDetailComponent,
-      MemberEditComponent
+      MemberEditComponent,
+      PhotoEditorComponent
    ],
    imports: [
       BrowserModule,
