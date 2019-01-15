6dce4a003b32d8664eed145f83cb76f2faba2fa5
Jayce77 <xjaycex@hotmail.com>
2019-01-14 23:04:08 -0500

    Added delete photo method to front end

--- DatingApp/DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp/DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -73,4 +73,16 @@ export class PhotoEditorComponent implements OnInit {
         this.alertify.error(error);
       });
   }
+
+  deletePhoto(id: number) {
+    this.alertify.confirm('Are you want to delete this photo?', () => {
+      this.userService.deletePhoto(this.authService.decodedToken.nameid, id)
+        .subscribe(() => {
+          this.photos = this.photos.filter(p => p.id !== id);
+          this.alertify.success('Photo has been deleted');
+        }, error => {
+          this.alertify.error('Failed to delete the photo');
+        });
+    });
+  }
 }
