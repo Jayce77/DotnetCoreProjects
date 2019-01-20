38a8e3a182a8350bd169af8d17ec6a233fa74df0
Jayce77 <xjaycex@hotmail.com>
2019-01-19 21:05:33 -0500

    Refactored register component to use formBuilder

--- DatingApp/DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,21 +14,29 @@ export class RegisterComponent implements OnInit {
   model: any = {};
   registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService,
+    private fb: FormBuilder) { }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      userName: ['', Validators.required],
+      password: [
+        '', 
+        [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(8)
+      ]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator
+    });
+  }
 
   ngOnInit() {
-    this.registerForm = 
-      new FormGroup({
-        userName: new FormControl('', Validators.required),
-        password: new FormControl(
-          '', [
-            Validators.required, 
-            Validators.minLength(4), 
-            Validators.maxLength(8)
-        ]),
-        confirmPassword: new FormControl('', Validators.required)
-        },
-        this.passwordMatchValidator);
+    this.createRegisterForm();
   }
 
   passwordMatchValidator(g: FormGroup) {
