b458273f63bd612e2e978ca429e4319717004447
Jayce77 <xjaycex@hotmail.com>
2019-01-23 21:18:50 -0500

    added helpers UserParams and PaginationHeaders

--- DatingApp/DatingApp.API/Helpers/Extensions.cs
+++ DatingApp/DatingApp.API/Helpers/Extensions.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace DatingApp.API.Helpers
 {
@@ -17,6 +18,14 @@ namespace DatingApp.API.Helpers
             var age = DateTime.Today.Year - theDateTime.Year;
             if (theDateTime.AddYears(age) > DateTime.Today) age--;
             return age;
+        }
+
+        public static void AddPagination(
+            this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
     }
 }
