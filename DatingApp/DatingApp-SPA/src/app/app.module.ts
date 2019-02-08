6d0ded8284dc3d6439ed0401685578433cb5db91
Jayce77 <xjaycex@hotmail.com>
2019-02-07 21:32:50 -0500

    added logic for retrieving meassages to client

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolvers';
+import { MessagesResolver } from './_resolvers/messages-resolver';
 
 export const tokenGetter = () => localStorage.getItem('token');
 
@@ -82,6 +83,7 @@ export const tokenGetter = () => localStorage.getItem('token');
       MemberDetailResolver,
       MemberListResolver,
       MemberEditResolver,
+      MessagesResolver,
       PreventUnsavedChanges
    ],
    bootstrap: [
