b118d9c84176b7a14a42119bae7b7219e06f5b8c
Jayce77 <xjaycex@hotmail.com>
2019-02-07 20:34:23 -0500

    Added endpoint and method to return message thread

--- DatingApp/DatingApp.API/Data/DatingRepository.cs
+++ DatingApp/DatingApp.API/Data/DatingRepository.cs
@@ -67,9 +67,17 @@ namespace DatingApp.API.Data
             return await PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
-            throw new NotImplementedException();
+            var messages = await _context.Messages
+                .Include(u => u.Sender).ThenInclude(p => p.Photos)
+                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+                .Where(m => m.SenderId == userId && m.RecipientId == recipientId
+                    || m.SenderId == recipientId && m.RecipientId == userId)
+                .OrderByDescending(m => m.MessageSent)
+                .ToListAsync();
+
+            return messages;
         }
 
         public async Task<Photo> GetPhoto(int id)
