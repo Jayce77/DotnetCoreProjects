c40617dcb13a99d075ad47ed15acb301b4e71d17
Jayce77 <xjaycex@hotmail.com>
2019-02-12 22:06:32 -0500

    added query params to angular route

--- DatingApp/DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp/DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +12,7 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs') memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -25,6 +27,11 @@ export class MemberDetailComponent implements OnInit {
       this.user = data['user'];
     });
 
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    });
+
     this.galleryOptions = [
       {
         width: '500px',
@@ -36,7 +43,11 @@ export class MemberDetailComponent implements OnInit {
       }
     ];
 
-    this.galleryImages = this.user.photos.map((photo, i) => {
+    this.galleryImages = this.getImages();
+  }
+
+  getImages() {
+    return this.user.photos.map((photo, i) => {
       return {
         small: this.user.photos[i].url,
         medium: this.user.photos[i].url,
@@ -44,5 +55,9 @@ export class MemberDetailComponent implements OnInit {
         description: this.user.photos[i].description,
       };
     });
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
